41cd54087b5d540c347e550df6eed92c676c9c53
caralocke <cara.m.locke@gmail.com>
2021-06-04 12:41:31 -0400

    Successfully completed Task 5 of card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,22 +1,45 @@
+// TASK 5
+// ---------------------
+// Implement this function, which should return the markup you see below.
+// It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
+// The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
+// The text inside elements will be set using their `textContent` property (NOT `innerText`).
+// Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
+//
+// <div class="card">
+//   <div class="headline">{ headline }</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={ authorPhoto }>
+//     </div>
+//     <span>By { authorName }</span>
+//   </div>
+// </div>
+//
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
+  const cardElement = document.createElement('div')
+  const headlineElement = document.createElement('div')
+  const authorElement = document.createElement('div')
+  const imgContainer = document.createElement('div')
+  const imgElement = document.createElement('img')
+  const authorName = document.createElement('span')
+
+  cardElement.appendChild(headlineElement)
+  cardElement.appendChild(authorElement)
+  authorElement.appendChild(imgContainer)
+  imgContainer.append(imgElement)
+  authorElement.appendChild(authorName)
+
+  cardElement.classList.add('card')
+  headlineElement.classList.add('headline')
+  authorElement.classList.add('author')
+  imgContainer.classList.add('img-container')
+
+  headlineElement.textContent = article.headline
+  imgElement.src = article.authorPhoto
+  authorName.textContent = `By ${article.authorName}`
+
+  return cardElement
 }
 
 const cardAppender = (selector) => {
